03d877ac7e8f169e0b0cfed4c980294bd753d0e7
Ford Fowler <fordjfowler@gmail.com>
2016-12-08 13:52:09 -0800

    implement sort_donors and send_thanks

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,1 +1,15 @@
-"""Mailroom."""
+"""Module to assist with donations."""
+
+
+DONORS = {}
+
+
+def send_thanks(donor_name):
+    """Send formatted email to donor."""
+    email = 'Thank you {} for your generous donation!'.format(donor_name)
+    return(email)
+
+
+def sort_donors(data=DONORS):
+    """Return sorted donor data."""
+    return sorted(data, key=lambda donor: data[donor]['total'], reverse=True)
--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -5,22 +5,17 @@ def test_send_thanks():
     """Test out output of thank email."""
     from mailroom import send_thanks
     email = send_thanks('bob')
-    assert email == 'Thanks bob for your generous donation!'
+    assert email == 'Thank you bob for your generous donation!'
 
 
 def test_sort_donors():
     """Testing if sort function sort by ammount."""
     from mailroom import sort_donors
     dict = {
-        'rick': {'history': [], 'total_donations': 21, 'avg_donation': 200},
-        'slim': {'history': [], 'total_donations': 213, 'avg_donation': 200},
-        'bob': {'history': [], 'total_donations': 54, 'avg_donation': 200},
-        'jack': {'history': [], 'total_donations': 3, 'avg_donation': 200}
+        'rick': {'history': [], 'total': 21, 'avg_donation': 200},
+        'slim': {'history': [], 'total': 213, 'avg_donation': 200},
+        'bob': {'history': [], 'total': 54, 'avg_donation': 200},
+        'jack': {'history': [], 'total': 3, 'avg_donation': 200}
     }
-    dict_sorted = {
-        'slim': {'history': [], 'total_donations': 213, 'avg_donation': 200},
-        'bob': {'history': [], 'total_donations': 54, 'avg_donation': 200},
-        'rick': {'history': [], 'total_donations': 21, 'avg_donation': 200},
-        'jack': {'history': [], 'total_donations': 3, 'avg_donation': 200}
-    }
-    assert dict_sorted == sort_donors(test=dict)
+    dict_sorted = ['slim', 'bob', 'rick', 'jack']
+    assert dict_sorted == sort_donors(data=dict)
