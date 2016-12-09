6a4f59d39131e25b7a87d85fca2858ca4b69cce6
Ford Fowler <fordjfowler@gmail.com>
2016-12-08 16:26:40 -0800

    separate user input for testing, define set_stats

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,23 +1,41 @@
 """Module to assist with donations."""
+import sys
 
 
 DONORS = {}
+QUIT = ['quit', 'exit', 'q']
+inp = input
 
 
 def main():
     """Main function that call other functions."""
     while True:
-        action = raw_input("Send a thank you or Create a report")
-        if action.lower() == "send a thank you":
-            donor_name = get_name()
-            DONORS.setdefault(donor_name, []).append(get_amount())
+        action = inp("\nSend thank you email or create report:  ")
+        if action.lower() in "send thank you":
+            donor_name = verify_name(get_name())
+            amount = verify_num(get_amount())
+            DONORS.setdefault(donor_name, []).append(amount)
+            set_stats(DONORS[donor_name])
             send_thanks(donor_name)
-        elif action.lower() == "create a report":
-            print(sorted_donors())
-        elif action.quit == "quit":
-            exit()
+        elif action.lower() in "create report":
+            print(format(sort_donors()))
+        elif action in QUIT:
+            sys.exit()
         else:
             print('Wrong Input')
+
+
+def set_stats(donor_name):
+    """Update total donations and avg donation for a donor."""
+    donor = DONORS[donor_name]
+    donor["total"] = sum(DONORS[donor_name]["history"])
+    donor["num_donations"] = len(donor["history"])
+    donor["avg_donation"] = donor["total"] / donor["num_donations"]
+
+
+def calc_avg(donor_name):
+    """Return total donations for a donor."""
+    return reduce(lambda x, y: x + y)
 
 
 def send_thanks(donor_name):
@@ -31,26 +49,46 @@ def sort_donors(data=DONORS):
     return sorted(data, key=lambda donor: data[donor]['total'], reverse=True)
 
 
-def get_name():
+def verify_name(name):
     """Return name from user."""
     while True:
-        input = raw_input("Donor's name (or 'list'): ")
+        input = inp("\nDonor's name (or 'list'):  ")
         if input.lower() == 'list':
             for donor in DONORS.keys():
                 print(donor)
-        elif input.lower() == 'quit':
+        elif input.lower() in QUIT:
             main()
         else:
             return input
+
+
+def get_name():
+    """Ask user for donor name."""
+    return inp("\nDonor's name (or 'list'):  ")
 
 
 def get_amount():
+    """Take user input."""
+    return inp("\n    Donation Amount:  ")
+
+
+def verify_num(input):
     """Return amount entered."""
-    while True:
-        input = raw_input("Donation Amount: ")
-        if type(input) is int:
-            return input
-        elif input.lower() == 'quit':
+    try:
+        input = float(input)
+    except ValueError:
+        if input.lower() in QUIT:
             main()
         else:
             print('Wrong Input')
+            return verify_num(get_amount())
+    else:
+        return input
+
+
+if __name__ == '__main__':  # pragma: no cover
+    try:
+        inp = raw_input
+    except NameError:
+        pass
+    main()
--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -1,4 +1,12 @@
 """Testing mailroom mudule."""
+import pytest
+import sys
+
+AMOUNT_INPUT = [
+    ['200', 200],
+    ['0', 0],
+    ['11.11', 11.11],
+]
 
 
 def test_send_thanks():
@@ -19,3 +27,17 @@ def test_sort_donors():
     }
     dict_sorted = ['slim', 'bob', 'rick', 'jack']
     assert dict_sorted == sort_donors(data=dict)
+
+
+@pytest.mark.parametrize('inp, output', AMOUNT_INPUT)
+def test_verify_num(inp, output):
+    """Test verify_num catches non-number input."""
+    from mailroom import verify_num
+    assert output == verify_num(inp)
+
+
+def test_invalid_verify_num():
+    """Verify verify_num raises exception if input is not a number."""
+    from mailroom import verify_num
+    with pytest.raises(Exception):
+        verify_num('NOTAnumber12e')
