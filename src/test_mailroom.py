bf9768f576deff3b0c43e0cee6ba39ea22462f8a
Amos Boldor <amosboldor@gmail.com>
2016-12-26 13:39:26 -0800

    removed empty test function

--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -34,10 +34,6 @@ REPORT = [
 ]
 
 
-def test_send_thanks():
-    """Functional test for send_thanks."""
-
-
 def test_format_email():
     """Test out output of thank email."""
     from mailroom import format_email
